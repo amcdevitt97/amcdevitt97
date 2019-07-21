826451a11fc0804f9b30b670364dcac01925b524
amcdevitt97 <amcdevitt97@gmail.com>
2019-07-20 19:12:03 -0500

    adding lost files

---
+++ precache-manifest.86870066af95c252f227e7122604dbb5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "94393125069eb17ba4eaf8b55133b7d8",
+    "url": "/static/media/linkedin.94393125.png"
+  },
+  {
+    "revision": "aef6fb684eb9fa27f8e0df0a5dc538c6",
+    "url": "/static/media/file.aef6fb68.png"
+  },
+  {
+    "revision": "1099b863cd4d3e98664734f6c999c2f2",
+    "url": "/static/media/McDevitt_Alyssa_Resume.1099b863.pdf"
+  },
+  {
+    "revision": "c98e7c55602d76fad7a2fc90f78f0c32",
+    "url": "/static/media/Black-Diamond.c98e7c55.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59c1213676cefe4954f5",
+    "url": "/static/js/main.760ea658.chunk.js"
+  },
+  {
+    "revision": "aaee8694848dd240c1e1",
+    "url": "/static/js/2.4077f0fd.chunk.js"
+  },
+  {
+    "revision": "59c1213676cefe4954f5",
+    "url": "/static/css/main.cc0e1b9e.chunk.css"
+  },
+  {
+    "revision": "4b52e2ee55c83ecc79e56d73e4a6f9ee",
+    "url": "/index.html"
+  }
+];
