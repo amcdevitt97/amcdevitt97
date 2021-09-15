e6d68e6c3b9692c9023b5e234ecde9125f9ea2c8
amcdevitt97 <amcdevitt97@gmail.com>
2021-09-15 17:50:35 -0400

    Updates

---
+++ precache-manifest.8ad3dfe1ebbba8f5e8bf458bf9cef9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dddf39cc086f72fcc6a7",
+    "url": "/static/css/main.fc89387f.chunk.css"
+  },
+  {
+    "revision": "dddf39cc086f72fcc6a7",
+    "url": "/static/js/main.67638d70.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "64337cf69cb91c74e984",
+    "url": "/static/js/2.a7b611b3.chunk.js"
+  },
+  {
+    "revision": "c98e7c55602d76fad7a2fc90f78f0c32",
+    "url": "/static/media/Black-Diamond.c98e7c55.woff"
+  },
+  {
+    "revision": "94393125069eb17ba4eaf8b55133b7d8",
+    "url": "/static/media/linkedin.94393125.png"
+  },
+  {
+    "revision": "764cce1208d1dfa5a481f9a66b985868",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7bd3a4b8335ed3135a3ed5994a84d16d.js"
+  "/precache-manifest.8ad3dfe1ebbba8f5e8bf458bf9cef9c8.js"
 );
 
 workbox.clientsClaim();
